8502397af771d9263abdc4a035d1588611e65a4a
AkkiSeven <akshaymadhira@gmail.com>
2020-12-26 09:35:38 +0530

    created google maps clone

---
+++ googlemaps-clone/script.js
@@ -0,0 +1,30 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWtraW1hcHMiLCJhIjoiY2tqNDRrd2NjMGg4MzJzbzluMTc0Mms4NCJ9.k2CnoS20OSHDpMtrg8popQ"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, { enableHighAccuracy: true })
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15
+  })
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  });
+
+  map.addControl(directions, 'top-left');
+}
+

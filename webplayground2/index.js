cb1098d08756bba82cc0bdb31b1012c4a62075e0
AkkiSeven <akshaymadhira@gmail.com>
2021-01-14 21:37:29 +0530

    created updated version for 'webplayground'

---
+++ webplayground2/index.js
@@ -0,0 +1,56 @@
+console.log("*****index.js*****");
+function update() {
+  var idoc = document.getElementById("iframe").contentWindow.document;
+
+  idoc.open();
+  idoc.write(editor.getValue());
+  idoc.close();
+}
+
+function setupEditor() {
+  window.editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/html");
+  editor.setValue(
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Your first program.</title>
+    </head>
+    <body>
+        <h1>Edit the HTML!</h1>
+        <h2> Don't bother about saving, live editing enabled!!!</h2>
+        <p>Hello World!!!🌍</p>
+        <style>
+            body {
+                font-family: sans-serif;
+            }
+        </style>
+    </body>
+    </html>`,
+    1
+  ); //1 = moves cursor to end
+
+  editor.getSession().on("change", function () {
+    update();
+  });
+
+  editor.focus();
+
+  editor.setOptions({
+    fontSize: "16pt",
+    showLineNumbers: true,
+    showGutter: true,
+    vScrollBarAlwaysVisible: true,
+    enableBasicAutocompletion: false,
+    enableLiveAutocompletion: false,
+  });
+
+  editor.setShowPrintMargin(false);
+  editor.setBehavioursEnabled(false);
+}
+
+function ready() {
+  setupEditor();
+  update();
+}
